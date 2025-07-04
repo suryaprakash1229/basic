2f7d5a0f1f1c0edd01331ae887da7a86195107cf
Suryaprakash <suryaprakashx0@gmail.com>
2025-07-04 21:19:46 +0530

    Add files via upload

---
+++ Basic1.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+class Basic1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine(); 
+        String b = sc.nextLine();  
+        System.out.print(a + " Technologies " + b); 
+        sc.close();  
+    }
+}
