4d8b98a842b8e43ebcd8e6e616107fcaf8079978
Suryaprakash <suryaprakashx0@gmail.com>
2025-07-04 21:31:38 +0530

    Add files via upload

---
+++ welcome.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class welcome{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String A=sc.nextLine();
+          System.out.print("welcome"+A);
+          sc.close();
+    }
+}
